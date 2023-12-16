992672176e67a7ca010f730542e000ea2b7a7337
jguimeram <jguimera54@gmail.com>
2023-12-16 12:30:02 +0100

    grid fixed

--- js/app.js
+++ js/app.js
@@ -9,38 +9,104 @@
     [ ] Color selector => onclick event
 */
 
-
 let entryPoint = document.querySelector('.entry__point')
+const range = document.querySelector('.range')
+const color = document.querySelector('.color')
+
 let grid
 let row
 let box
 
-const createGrid = function () {
+const boxSize = {
+    numberOfCells: 16,
+    gridWidth: 400,
+    width: "1",
+    height: "1",
+}
+
+function createGrid() {
     grid = document.createElement('div')
     grid.classList.add('grid')
     entryPoint.appendChild(grid)
 }
 
-const createRow = function () {
+function createRow() {
     row = document.createElement('div')
     row.classList.add('row')
     grid.appendChild(row)
 }
-const createBox = function () {
+function createBox() {
     box = document.createElement('div')
     box.classList.add('box')
+    box.style.width = (boxSize.gridWidth / boxSize.numberOfCells).toString() + "px"
+    box.style.height = (boxSize.gridWidth / boxSize.numberOfCells).toString() + "px"
     row.appendChild(box)
+
 }
 
+function drawGrid() {
 
-createGrid()
 
-for (let i = 0; i < 16; i++) {
-    createRow()
-    for (let j = 0; j < 16; j++) {
-        createBox()
+    for (let i = 0; i < boxSize.numberOfCells; i++) {
+        createRow()
+        for (let j = 0; j < boxSize.numberOfCells; j++) {
+            createBox()
+        }
     }
 }
+
+function removeGrid() {
+    grid.textContent = ""
+}
+
+function dataIDinBoxes() {
+    const boxes = document.querySelectorAll('.box')
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].setAttribute('data-id', i)
+    }
+}
+
+function paint() {
+    const grid = document.querySelector('.grid')
+    grid.addEventListener('click', (e) => {
+        if (e.target.classList.contains('box')) {
+            changeColor(e)
+        }
+    })
+}
+
+function changeColor(arg) {
+    const currentColor = document.querySelector('.color')
+    if (arg.target.style.backgroundColor == "") arg.target.style.backgroundColor = currentColor.value
+    else arg.target.style.backgroundColor = ""
+}
+
+function changeGrid() {
+
+    range.addEventListener('input', (e) => {
+        boxSize.numberOfCells = Number(e.target.value);
+        boxSize.width = boxSize.gridWidth / Number(e.target.value);
+        boxSize.height = boxSize.gridWidth / Number(e.target.value);
+        removeGrid()
+        drawGrid()
+    })
+
+}
+
+(() => {
+    createGrid()
+    drawGrid()
+    dataIDinBoxes()
+    paint()
+    changeGrid()
+})()
+
+
+
+
+
+
+
 
 
 
